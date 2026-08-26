9fe24e15296dbdb1ff1c02480f2abaa2aa7cf394
SRIRAM MOOKKANDI <sriram.mookkandi@gmail.com>
2026-08-26 20:00:04 +0530

    Features/gpt search (#7)
    * Features/home (#5) (#6)
    
    * Trailer logic created
    
    * MovieTrailer feature updated
    
    * Chore: Secondary container updated
    
    * Home page completed
    
    * Chore: Gpt button added
    
    * chore: File structure updated

--- src/components/home/secondaryContainer/MovieCard.jsx
+++ src/components/home/secondaryContainer/MovieCard.jsx
@@ -1,8 +1,9 @@
 import { CDN_IMG_URL } from "../../../utils/constants"
+import { Link } from "react-router-dom"
 const MovieCard = ({ posterPath }) => {
     return (
-        <div className="w-48 px-2">
-            <img alt='moviecard' src={CDN_IMG_URL + posterPath}></img>
+        <div className="w-48 px-2 duration-300 ease-in-out hover:scale-110 ">
+            <Link> <img alt='moviecard' src={CDN_IMG_URL + posterPath}></img></Link>
         </div>
     )
 }
